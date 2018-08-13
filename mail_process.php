e8bf32bd6dc2776a229a9318741e34adc2858380
erinstory <erin.kang1992@gmail.com>
2018-08-13 17:27:04 +0900

    Update mail_process.php

--- mail_process.php
+++ mail_process.php
@@ -11,18 +11,18 @@
 	$mail = new PHPMailer;
 	# $mail->CharSet = 'utf-8'; /// 언어셋 설정
 	# $mail->Encoding = 'base64'; /// 인코딩 방법 정의
-	$mail->CharSet = "EUC-KR";
+	$mail->CharSet = "UTF-8";
 	$mail->Encoding = "base64";
 	$mail->isSMTP();
-	$mail->SMTPDebug = 2;
-	$mail->Host = 'smtp.gmail.com';    ////
+//	$mail->SMTPDebug = 2;
+	$mail->Host = 'smtp.naver.com';    ////
 	$mail->Port = 465;  ////
 	$mail->SMTPSecure = 'ssl';  ////
 	$mail->SMTPAuth = true;
-	$mail->Username ="erin.kang1020@gmail.com";		 //// 관리자 naver 아이디
+	$mail->Username ="yjprogrammar@naver.com";		 //// 관리자 naver 아이디
 	$mail->Password = "Erin1142002!";	 //// 관리자 naver 비밀번호
-	$mail->setFrom('erin.kang1020@gmail.com',  $sender_name  );    ////보내는 사람정보
-	$mail->addAddress('erin.kang1020@gmail.com', 'admin'); ////받는 사람정보
+	$mail->setFrom('yjprogrammar@naver.com',  $sender_name  );    ////보내는 사람정보
+	$mail->addAddress('yjprogrammar@naver.com', 'admin'); ////받는 사람정보
 	$mail->isHTML(true);   //// Set email format to HTML
 	$mail->Subject = $sender_title  ;  ////제목
 	$mail->Body = $sender_content  ; ////내용
